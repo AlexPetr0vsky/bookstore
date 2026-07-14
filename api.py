1562ee426a455fb0ecc424363f7d7bbae56b8936
AlexPetr0vsky <dr.alexpetrovsky@yandex.ru>
2026-07-14 17:48:57 +0500

    Added delete and post methods

--- api.py
+++ api.py
@@ -26,7 +26,13 @@ def handle_books(db):
         )
         db.add_all([author, book])
         db.commit()
-        return jsonify({'success': 'OK'}), 200
+        return jsonify({
+            'id': book.id,
+            'book': book.book,
+            'description': book.description,
+            'icon_book': book.icon_book,
+            'author_id': book.author_id
+        }), 201
 
     books = db.query(Book).all()
     result = []
@@ -54,3 +60,42 @@ def get_book_by_id(db, book_id):
         'icon_book': book.icon_book,
         'author_id': book.author_id
     })
+
+
+@app.route('/api/books/<int:book_id>', methods=['DELETE'])
+@with_session
+def delete_book(db, book_id):
+    book = db.query(Book).filter_by(id=book_id).first()
+    if not book:
+        return jsonify({'error': 'Book not found'}), 404
+    db.delete(book)
+    db.commit()
+    return '', 204
+
+
+@app.route('/api/authors', methods=['GET'])
+@with_session
+def get_authors_api(db):
+    authors = db.query(Author).all()
+    return jsonify([{
+        'id': a.id,
+        'name': a.name,
+        'photo': a.photo,
+        'wiki': a.wiki
+    } for a in authors])
+
+
+@app.route('/api/authors/<int:author_id>', methods=['DELETE'])
+@with_session
+def delete_author_api(db, author_id):
+    author = db.query(Author).filter_by(id=author_id).first()
+    if not author:
+        return jsonify({'error': 'Author not found'}), 404
+
+    books = db.query(Book).filter_by(author_id=author_id).all()
+    for book in books:
+        db.delete(book)
+
+    db.delete(author)
+    db.commit()
+    return '', 204
