e957a0fa11bafda0cb68101f548f2fbd2bd6939a
AlexPetr0vsky <dr.alexpetrovsky@yandex.ru>
2026-08-19 19:32:48 +0500

    Added git compose and wibecoded new api

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 from flask import request, jsonify
 from db_setup import Book, Author
 from app import app, with_session
+from sqlalchemy import exc
 
 
 @app.route('/api/books', methods=['GET', 'POST'])
@@ -9,22 +10,78 @@ def handle_books(db):
     if request.method == 'POST':
         if not request.json:
             return jsonify({'error': 'Empty request'}), 400
-        if not all(key in request.json for key in ['book']):
+        if not all(key in request.json for key in ['book', 'name']):
             return jsonify({'error': 'Bad request'}), 400
 
-        author = Author(
-            name=request.json['name'],
-            photo=request.json['photo'],
-            wiki=request.json['wiki']
-        )
-        author_id = db.query(Author.id).filter(Author.name == request.json['name'])
+        author = db.query(Author).filter_by(name=request.json['name']).first()
+        if not author:
+            author = Author(
+                name=request.json['name'],
+                photo=request.json.get('photo'),
+                wiki=request.json.get('wiki')
+            )
+            db.add(author)
+            db.flush()
+
         book = Book(
             book=request.json['book'],
-            description=request.json['description'],
-            icon_book=request.json['icon_book'],
-            author_id=author_id
+            description=request.json.get('description', ''),
+            icon_book=request.json.get('icon_book', ''),
+            author_id=author.id
         )
-        db.add_all([author, book])
+        db.add(book)
+        db.commit()
+        return jsonify({
+            'id': book.id,
+            'book': book.book,
+            'description': book.description,
+            'icon_book': book.icon_book,
+            'author_id': author.id
+        }), 201
+
+    books = db.query(Book).all()
+    return jsonify([{
+        'id': b.id,
+        'book': b.book,
+        'description': b.description,
+        'icon_book': b.icon_book,
+        'author_id': b.author_id
+    } for b in books]), 200
+
+
+@app.route('/api/books/<int:book_id>', methods=['GET', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'])
+@with_session
+def handle_book(db, book_id):
+    book = db.query(Book).filter_by(id=book_id).first()
+    if not book:
+        return jsonify({'error': 'Book not found'}), 404
+
+    if request.method == 'GET':
+        return jsonify({
+            'id': book.id,
+            'book': book.book,
+            'description': book.description,
+            'icon_book': book.icon_book,
+            'author_id': book.author_id
+        }), 200
+
+    if request.method == 'PUT' or request.method == 'PATCH':
+        if not request.json:
+            return jsonify({'error': 'Empty request'}), 400
+
+        data = request.json
+        if 'book' in data:
+            book.book = data['book']
+        if 'description' in data:
+            book.description = data['description']
+        if 'icon_book' in data:
+            book.icon_book = data['icon_book']
+        if 'author_id' in data:
+            author = db.query(Author).filter_by(id=data['author_id']).first()
+            if not author:
+                return jsonify({'error': 'Author not found'}), 404
+            book.author_id = data['author_id']
+
         db.commit()
         return jsonify({
             'id': book.id,
@@ -32,70 +89,97 @@ def handle_books(db):
             'description': book.description,
             'icon_book': book.icon_book,
             'author_id': book.author_id
+        }), 200
+
+    if request.method == 'DELETE':
+        db.delete(book)
+        db.commit()
+        return '', 204
+
+    if request.method == 'HEAD':
+        return '', 200
+
+    if request.method == 'OPTIONS':
+        return '', 200
+
+
+@app.route('/api/authors', methods=['GET', 'POST'])
+@with_session
+def handle_authors(db):
+    if request.method == 'POST':
+        if not request.json:
+            return jsonify({'error': 'Empty request'}), 400
+        if 'name' not in request.json:
+            return jsonify({'error': 'Bad request'}), 400
+
+        author = Author(
+            name=request.json['name'],
+            photo=request.json.get('photo', ''),
+            wiki=request.json.get('wiki', '')
+        )
+        db.add(author)
+        db.commit()
+        return jsonify({
+            'id': author.id,
+            'name': author.name,
+            'photo': author.photo,
+            'wiki': author.wiki
         }), 201
 
-    books = db.query(Book).all()
-    result = []
-    for book in books:
-        result.append({
-            'id': book.id,
-            'book': book.book,
-            'description': book.description,
-            'icon_book': book.icon_book,
-            'author_id': book.author_id
-        })
-    return jsonify(result), 200
-
-
-@app.route('/api/books/<int:book_id>', methods=['GET'])
-@with_session
-def get_book_by_id(db, book_id):
-    book = db.query(Book).filter_by(id=book_id).first()
-    if not book:
-        return jsonify({'error': 'Book not found'}), 404
-    return jsonify({
-        'id': book.id,
-        'book': book.book,
-        'description': book.description,
-        'icon_book': book.icon_book,
-        'author_id': book.author_id
-    })
-
-
-@app.route('/api/books/<int:book_id>', methods=['DELETE'])
-@with_session
-def delete_book(db, book_id):
-    book = db.query(Book).filter_by(id=book_id).first()
-    if not book:
-        return jsonify({'error': 'Book not found'}), 404
-    db.delete(book)
-    db.commit()
-    return '', 204
-
-
-@app.route('/api/authors', methods=['GET'])
-@with_session
-def get_authors_api(db):
     authors = db.query(Author).all()
     return jsonify([{
         'id': a.id,
         'name': a.name,
         'photo': a.photo,
         'wiki': a.wiki
-    } for a in authors])
+    } for a in authors]), 200
 
 
-@app.route('/api/authors/<int:author_id>', methods=['DELETE'])
+@app.route('/api/authors/<int:author_id>', methods=['GET', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'])
 @with_session
-def delete_author_api(db, author_id):
+def handle_author(db, author_id):
     author = db.query(Author).filter_by(id=author_id).first()
     if not author:
         return jsonify({'error': 'Author not found'}), 404
 
-    books = db.query(Book).filter_by(author_id=author_id).all()
-    for book in books:
-        db.delete(book)
+    if request.method == 'GET':
+        return jsonify({
+            'id': author.id,
+            'name': author.name,
+            'photo': author.photo,
+            'wiki': author.wiki
+        }), 200
 
-    db.delete(author)
-    db.commit()
-    return '', 204
+    if request.method == 'PUT' or request.method == 'PATCH':
+        if not request.json:
+            return jsonify({'error': 'Empty request'}), 400
+
+        data = request.json
+        if 'name' in data:
+            author.name = data['name']
+        if 'photo' in data:
+            author.photo = data['photo']
+        if 'wiki' in data:
+            author.wiki = data['wiki']
+
+        db.commit()
+        return jsonify({
+            'id': author.id,
+            'name': author.name,
+            'photo': author.photo,
+            'wiki': author.wiki
+        }), 200
+
+    if request.method == 'DELETE':
+        books = db.query(Book).filter_by(author_id=author_id).all()
+        for book in books:
+            db.delete(book)
+        db.delete(author)
+        db.commit()
+        return '', 204
+
+    if request.method == 'HEAD':
+        return '', 200
+
+    if request.method == 'OPTIONS':
+        return '', 200
