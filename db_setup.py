306a1261868b9d03ff5e2e25944bbe0492b22c58
AlexPetr0vsky <dr.alexpetrovsky@yandex.ru>
2026-09-01 21:49:44 +0500

    Fixed atribute error issues

--- db_setup.py
+++ db_setup.py
@@ -2,7 +2,7 @@ from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy_serializer import SerializerMixin
-
+from werkzeug.security import generate_password_hash, check_password_hash
 
 Base = declarative_base()
 
@@ -34,3 +34,24 @@ class User(Base, SerializerMixin):
 
     def __repr__(self):
         return '<User {}>'.format(self.name)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return str(self.id)
